c56f727099a74017360315be9e117aed5da2351e
AShabunevich <56523105+AShabunevich@users.noreply.github.com>
2020-12-07 18:43:44 -0700

    Update and rename adventocode_day1.py to adventofcode_day1.py

--- adventofcode_day1.py
+++ adventofcode_day1.py
@@ -3,12 +3,16 @@ import numpy as np
 array = np.loadtxt('input1')
 
 for i in range(len(array)):
+    
     for j in range(i+1, len(array)):
+        
         if array[i] + array[j] == 2020:
             print(str(array[i]) + ' + ' + str(array[j]) + ' = 2020')
             print(str(array[i]) + ' * ' + str(array[j]) +
                   ' = ' + str(array[i] * array[j]))
+            
         for k in range(j+1, len(array)):
+            
             if array[i] + array[j] + array[k] == 2020:
                 print(str(array[i]) + ' + ' + str(array[j]) +
                       ' + ' + str(array[k]) + ' = 2020')
